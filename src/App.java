be2a2aec3a75eb5e7f332f68835252aa1645f999
Mateusz <bmatteo@interia.pl>
2024-05-27 12:33:37 +0200

    init

---
+++ src/App.java
@@ -0,0 +1,44 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Witamy na szkoleniu !!!");
+        System.out.println("jakis nasz tekst na ekranie !!");
+
+        int liczba;
+        String imie;
+        boolean logiczna;
+
+        short malaLiczba = 1;
+
+        int normalnaLiczba = 30000;
+
+        long duzaLiczbaCalkowita = 200000000000L;
+        long duzaLiczba2 = 200000;
+
+        int someBigIntegerNumber = 1000;
+
+        double liczbaZmiennoprzecinkowa = 4.4;
+        float liczbaZmiennoprzecinkowa2 = 5.5f;
+
+        char znak = 'r';
+        String tekst = "r";
+
+        boolean wartoscLogiczna = false;
+
+        int x = 5;
+        double y = x;
+
+        double d = 5.5;
+        int i = (int) d;
+
+        //String s = "6";
+        //int i2 = (int) s;
+
+        //comment
+        int cos;
+        /*
+        kahjsdgfjkh
+        alskjdghf
+        askjdhgf
+         */
+    }
+}
---
+++ src/App2.java
@@ -0,0 +1,34 @@
+public class App2 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+
+        System.out.println(a % b);
+
+        String imie = "Mateusz";
+        String nazwisko = "Bereda";
+
+        System.out.println(imie + " " + nazwisko);
+
+        boolean wynik = a >= b;
+        System.out.println(wynik);
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println(!b1);
+
+        int x = 5 + 3 * 2;
+        System.out.println(x);
+
+        x++;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+
+        x = x + 5;
+        System.out.println(x);
+        x+=5;
+        System.out.println(x);
+    }
+}
